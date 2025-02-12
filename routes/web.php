fb3035754bf9832194445cc907a4afdda7ce929e
NomieC <118948158+NomieC@users.noreply.github.com>
2025-02-12 10:41:55 +0700

    main,navbar

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,12 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
+    return Inertia::render('Main', [
+
+    ]);
+});
+
+Route::get('/Welcome', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
